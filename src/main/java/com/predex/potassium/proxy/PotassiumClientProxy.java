24e9381d8be70ef946485bb93ec0150e188055fa
itzpredex373-lang <itzpredex373@gmail.com>
2026-09-23 12:16:55 +0600

    Register Part 4 system performance scheduler

--- src/main/java/com/predex/potassium/proxy/PotassiumClientProxy.java
+++ src/main/java/com/predex/potassium/proxy/PotassiumClientProxy.java
@@ -4,6 +4,7 @@ import com.predex.potassium.optimization.rendering.EntityRenderOptimizer;
 import com.predex.potassium.optimization.entities.EntityUpdateHandler;
 import com.predex.potassium.optimization.particles.ParticleClientScheduler;
 import com.predex.potassium.optimization.chunks.ChunkRenderScheduler;
+import com.predex.potassium.optimization.system.SystemPerformanceScheduler;
 import net.minecraftforge.common.MinecraftForge;
 
 /**
@@ -12,6 +13,7 @@ import net.minecraftforge.common.MinecraftForge;
 public final class PotassiumClientProxy extends PotassiumProxy {
     @Override
     public void registerClientHooks() {
+        MinecraftForge.EVENT_BUS.register(new SystemPerformanceScheduler());
         MinecraftForge.EVENT_BUS.register(new EntityRenderOptimizer());
         MinecraftForge.EVENT_BUS.register(new EntityUpdateHandler());
         MinecraftForge.EVENT_BUS.register(new ParticleClientScheduler());
