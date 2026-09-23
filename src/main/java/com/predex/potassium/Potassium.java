8935a6462e78c4797477b3bcefa2300b691022d5
itzpredex373-lang <itzpredex373@gmail.com>
2026-09-23 11:58:31 +0600

    Initialize Potassium Forge 1.8.9 project

---
+++ src/main/java/com/predex/potassium/Potassium.java
@@ -0,0 +1,31 @@
+package com.predex.potassium;
+
+import com.predex.potassium.config.PotassiumConfig;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Potassium.MOD_ID, name = Potassium.NAME, version = Potassium.VERSION, acceptedMinecraftVersions = "[1.8.9]")
+public final class Potassium {
+    public static final String MOD_ID = "potassium";
+    public static final String NAME = "Potassium";
+    public static final String VERSION = "0.1.0";
+
+    private static Potassium instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        instance = this;
+        PotassiumConfig.init(event.getSuggestedConfigurationFile());
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        MinecraftForge.EVENT_BUS.register(new PotassiumEventHandler());
+    }
+
+    public static Potassium getInstance() {
+        return instance;
+    }
+}
