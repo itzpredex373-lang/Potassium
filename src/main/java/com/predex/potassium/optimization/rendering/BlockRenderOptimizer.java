994c977d880f5a61f7a9664a5afcc2a3e4310504
itzpredex373-lang <itzpredex373@gmail.com>
2026-09-23 12:02:22 +0600

    Add rendering optimization structure

---
+++ src/main/java/com/predex/potassium/optimization/rendering/BlockRenderOptimizer.java
@@ -0,0 +1,13 @@
+package com.predex.potassium.optimization.rendering;
+
+/**
+ * Reserved for block-rendering optimizations.
+ *
+ * Future work:
+ * - reduce unnecessary block render work
+ * - improve render-pass decisions
+ * - avoid redundant rebuild requests
+ */
+public final class BlockRenderOptimizer {
+    private BlockRenderOptimizer() {}
+}
