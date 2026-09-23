8567a0200b3704cbf87e1a3456f61ef07a886a8d
itzpredex373-lang <itzpredex373@gmail.com>
2026-09-23 18:10:24 +0600

    Improve Part A entity render culling

--- src/main/java/com/predex/potassium/optimization/rendering/EntityRenderOptimizer.java
+++ src/main/java/com/predex/potassium/optimization/rendering/EntityRenderOptimizer.java
@@ -3,11 +3,6 @@ package com.predex.potassium.optimization.rendering;
 import net.minecraftforge.client.event.RenderLivingEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-/**
- * Entity rendering optimization hooks.
- *
- * Distance and frustum tests run before normal living-entity renderer work.
- */
 public final class EntityRenderOptimizer {
     @SubscribeEvent
     public void onRenderLivingPre(RenderLivingEvent.Pre event) {
