4ed53c5407eb6b065587e463d20f8c47a35290aa
itzpredex373-lang <itzpredex373@gmail.com>
2026-09-24 22:43:34 +0600

    Remove developer command and debug overlay registration

--- src/main/java/com/predex/potassium/proxy/PotassiumClientProxy.java
+++ src/main/java/com/predex/potassium/proxy/PotassiumClientProxy.java
@@ -5,11 +5,8 @@ import com.predex.potassium.optimization.entities.EntityUpdateHandler;
 import com.predex.potassium.optimization.particles.ParticleClientScheduler;
 import com.predex.potassium.optimization.chunks.ChunkRenderScheduler;
 import com.predex.potassium.optimization.system.SystemPerformanceScheduler;
-import com.predex.potassium.optimization.benchmark.PotassiumDebugOverlay;
 import com.predex.potassium.PotassiumEventHandler;
-import com.predex.potassium.dev.PotassiumDevCommand;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.client.ClientCommandHandler;
 
 public final class PotassiumClientProxy extends PotassiumProxy {
     @Override
@@ -20,7 +17,5 @@ public final class PotassiumClientProxy extends PotassiumProxy {
         MinecraftForge.EVENT_BUS.register(new EntityUpdateHandler());
         MinecraftForge.EVENT_BUS.register(new ParticleClientScheduler());
         MinecraftForge.EVENT_BUS.register(new ChunkRenderScheduler());
-        MinecraftForge.EVENT_BUS.register(new PotassiumDebugOverlay());
-        ClientCommandHandler.instance.registerCommand(new PotassiumDevCommand());
     }
 }
