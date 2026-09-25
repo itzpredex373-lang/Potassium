0af438159669aed64f84b343d17d26acf31a803c
itzpredex373-lang <itzpredex373@gmail.com>
2026-09-26 02:43:16 +0600

    Register client-only mini pet renderer

--- src/main/java/com/predex/potassium/proxy/PotassiumClientProxy.java
+++ src/main/java/com/predex/potassium/proxy/PotassiumClientProxy.java
@@ -2,8 +2,7 @@ package com.predex.potassium.proxy;
 
 import com.predex.potassium.client.PotassiumGuiHandler;
 import com.predex.potassium.client.PotassiumQolHud;
-import com.predex.potassium.client.RenderPotassiumPet;
-import com.predex.potassium.pet.EntityPotassiumPet;
+import com.predex.potassium.client.PotassiumMiniPet;
 import com.predex.potassium.optimization.rendering.EntityRenderOptimizer;
 import com.predex.potassium.optimization.entities.EntityUpdateHandler;
 import com.predex.potassium.optimization.particles.ParticleClientScheduler;
@@ -11,7 +10,6 @@ import com.predex.potassium.optimization.chunks.ChunkRenderScheduler;
 import com.predex.potassium.optimization.system.SystemPerformanceScheduler;
 import com.predex.potassium.PotassiumEventHandler;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.client.registry.RenderingRegistry;
 
 public final class PotassiumClientProxy extends PotassiumProxy {
     @Override
@@ -24,7 +22,7 @@ public final class PotassiumClientProxy extends PotassiumProxy {
         MinecraftForge.EVENT_BUS.register(new ChunkRenderScheduler());
         MinecraftForge.EVENT_BUS.register(new PotassiumGuiHandler());
         MinecraftForge.EVENT_BUS.register(new PotassiumQolHud());
-        RenderingRegistry.registerEntityRenderingHandler(EntityPotassiumPet.class, RenderPotassiumPet::new);
+        MinecraftForge.EVENT_BUS.register(new PotassiumMiniPet());
         PotassiumPetKeyHandler petKeyHandler = new PotassiumPetKeyHandler();
         petKeyHandler.register();
         MinecraftForge.EVENT_BUS.register(petKeyHandler);
