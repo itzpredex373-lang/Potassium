ba9b2dcb6ffcdd0e36e3719b1527a02669bd505b
itzpredex373-lang <itzpredex373@gmail.com>
2026-09-26 02:38:25 +0600

    Add common proxy registration hook

--- src/main/java/com/predex/potassium/proxy/PotassiumProxy.java
+++ src/main/java/com/predex/potassium/proxy/PotassiumProxy.java
@@ -4,6 +4,10 @@ package com.predex.potassium.proxy;
  * Common proxy. Client-only classes are kept out of the common bootstrap path.
  */
 public class PotassiumProxy {
+    public void registerCommonHooks() {
+        // Common/server registration lives here.
+    }
+
     public void registerClientHooks() {
         // No-op on dedicated servers.
     }
