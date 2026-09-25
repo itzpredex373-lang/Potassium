214111e46e6654f0f05b5d8692f5790007fdc351
itzpredex373-lang <itzpredex373@gmail.com>
2026-09-25 06:44:30 +0600

    Add clear Potassium startup log messages

--- src/main/java/com/predex/potassium/Potassium.java
+++ src/main/java/com/predex/potassium/Potassium.java
@@ -4,6 +4,8 @@ import com.predex.potassium.config.PotassiumConfig;
 import com.predex.potassium.proxy.PotassiumProxy;
 import com.predex.potassium.optimization.profile.PerformanceProfileManager;
 import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -13,6 +15,7 @@ public final class Potassium {
     public static final String MOD_ID = "potassium";
     public static final String NAME = "Potassium";
     public static final String VERSION = "0.1.3";
+    private static final Logger LOGGER = LogManager.getLogger("Potassium");
 
     @SidedProxy(
             clientSide = "com.predex.potassium.proxy.PotassiumClientProxy",
@@ -26,11 +29,13 @@ public final class Potassium {
         instance = this;
         PotassiumConfig.init(event.getSuggestedConfigurationFile());
         PerformanceProfileManager.applyConfiguredProfile();
+        LOGGER.info("Potassium {} initializing for Minecraft 1.8.9", VERSION);
     }
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
         proxy.registerClientHooks();
+        LOGGER.info("Potassium {} client optimization hooks enabled", VERSION);
     }
 
     public static Potassium getInstance() {
