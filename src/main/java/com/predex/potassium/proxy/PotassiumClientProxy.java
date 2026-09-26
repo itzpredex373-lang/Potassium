d1ad8609233d3c341bfb02b04bee2db6b6ace9ac
itzpredex373-lang <itzpredex373@gmail.com>
2026-09-27 01:47:21 +0600

    feat: initialize pet packs on client startup

--- src/main/java/com/predex/potassium/proxy/PotassiumClientProxy.java
+++ src/main/java/com/predex/potassium/proxy/PotassiumClientProxy.java
@@ -3,6 +3,7 @@ package com.predex.potassium.proxy;
 import com.predex.potassium.client.PotassiumGuiHandler;
 import com.predex.potassium.client.PotassiumQolHud;
 import com.predex.potassium.client.PotassiumMiniPet;
+import com.predex.potassium.client.PotassiumPetPackManager;
 import com.predex.potassium.client.PotassiumPetKeyHandler;
 import com.predex.potassium.client.PotassiumOwnNameTag;
 import com.predex.potassium.optimization.rendering.EntityRenderOptimizer;
@@ -16,6 +17,7 @@ import net.minecraftforge.common.MinecraftForge;
 public final class PotassiumClientProxy extends PotassiumProxy {
     @Override
     public void registerClientHooks() {
+        PotassiumPetPackManager.init();
         MinecraftForge.EVENT_BUS.register(new PotassiumEventHandler());
         MinecraftForge.EVENT_BUS.register(new SystemPerformanceScheduler());
         MinecraftForge.EVENT_BUS.register(new EntityRenderOptimizer());
